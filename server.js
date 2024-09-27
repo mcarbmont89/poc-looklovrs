1ca2b7f886d61290a04bf9c8b4a6108fb6cdabb7
Lautaro Amengual <amengual.ln@gmail.com>
2024-09-27 10:49:55 -0300

    poc mvp

--- server.js
+++ server.js
@@ -1,1 +1,83 @@
-// we will fill out this file during the tutorial
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+require('dotenv').config()
+
+const fs = require('fs')
+const multer = require('multer')
+const { OpenAI } = require('openai')
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage }).single('file')
+let filePath
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.status(500).json(err)
+        }
+        filePath = req.file.path
+        console.log(filePath)
+    })
+})
+
+app.post('/vision', async (req, res) => {
+    try {
+        console.log('sent to vision')
+        // const imageAsBase64 = fs.readFileSync("public/1715260546477-mihaly2222-koles-5V-W4zTqZ74-unsplash.jpg", 'base64')
+        // const image_url = `data:image/jpeg;base64,${fs.readFileSync("public/1715260546477-mihaly2222-koles-5V-W4zTqZ74-unsplash.jpg", 'base64')}`
+        const response = await openai.chat.completions.create({
+            model: "gpt-4-vision-preview",
+            messages: [
+                {
+                    role: "user",
+                    content: [
+                        {
+                            type: "image_url",
+                            image_url: `data:image/jpeg;base64,${fs.readFileSync("public/resized1.jpeg", 'base64')}`
+                        },
+                        {
+                            type: "image_url",
+                            image_url: `data:image/jpeg;base64,${fs.readFileSync("public/resized2.jpeg", 'base64')}`
+                        },
+                        {
+                            type: "image_url",
+                            image_url: `data:image/jpeg;base64,${fs.readFileSync("public/resized3.jpeg", 'base64')}`
+                        },
+                        {
+                            type: "image_url",
+                            image_url: `data:image/jpeg;base64,${fs.readFileSync("public/resized4.jpeg", 'base64')}`
+                        },
+                        {
+                            type: "image_url",
+                            image_url: `data:image/jpeg;base64,${fs.readFileSync("public/resized5.jpeg", 'base64')}`
+                        },
+                        { type: "text", text: "Crea las mejores combinaciones para vestirme con la ropa de mi guardarropas. Dame la respuesta en un formato simple pero bonito para mostrar" },
+                        { type: "text", text: req.body.message },
+                    ],
+                },
+            ],
+        });
+        res.send(response.choices[0].message.content)
+    }
+    catch (error) {
+        console.error(error)
+    }
+})
+
+app.listen(8000, () => `App listening on port 8000`)
--- src/App.js
+++ src/App.js
@@ -1,45 +1,105 @@
-import { useState } from "react"
+import { useEffect, useState } from 'react'
+
+import { SendIcon } from './components/Icons';
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper/modules'
+import 'swiper/css';
+
+import { ScaleLoader } from 'react-spinners'
 
 const App = () => {
-  const [image, setImage] = useState(null)
-  const [value, setValue] = useState("")
-  const [response, setResponse] = useState("")
-  const [error, setError] = useState("")
+  const [value, setValue] = useState('')
+  const [response, setResponse] = useState('')
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  const surpriseOptions = [
-    'Does the image have a whale?',
-    'Is the image fabulously pink?',
-    'Does the image have puppies?'
-  ]
+  const analyzeImage = async () => {
+    setResponse('')
+    setLoading(true)
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          message: value
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }
+      const response = await fetch('http://localhost:8000/vision', options)
+      const data = await response.text()
+      setResponse(data)
+    }
+    catch (error) {
+      console.error(error)
+    }
+    finally {
+      setLoading(false)
+    }
+  }
 
+  useEffect(() => {
+    clearExceptInput()
+  }, [value])
+
+  const clear = () => {
+    setValue('')
+    setResponse('')
+    setError('')
+  }
+
+  const clearExceptInput = () => {
+    setResponse('')
+    setError('')
+  }
 
   return (
-    <div className="app">
-      <section className="search-section">
-        <div className="image-container">
-          {image && <img className="image" src={""} />}
+    <div className='app'>
+      <section className={`flex justify-center transform h-[250px]`}>
+        <img src="logo.png" alt="Logo" />
+      </section>
+      <section className='search-section'>
+        <h1 className='text-xl pl-6 text-gray-400 mb-2'>Armario: </h1>
+        <div className='h-[320px] min-w-[320px] mx-auto px-6'>
+          <Swiper
+            modules={[Autoplay]}
+            slidesPerView={1}
+            autoplay={{ autoplay: 1000 }}
+            loop
+          >
+            <SwiperSlide className='rounded-lg bg-[url("../public/wardrobe/remera1.png")] bg-cover bg-center max-h-[320px] min-w-[320px]'></SwiperSlide>
+            <SwiperSlide className='rounded-lg bg-[url("../public/wardrobe/sudadera2.png")] bg-cover bg-center max-h-[320px] min-w-[320px]'></SwiperSlide>
+            <SwiperSlide className='rounded-lg bg-[url("../public/wardrobe/blusa3.png")] bg-cover bg-center max-h-[320px] min-w-[320px]'></SwiperSlide>
+            <SwiperSlide className='rounded-lg bg-[url("../public/wardrobe/pantalon4.png")] bg-cover bg-center max-h-[320px] min-w-[320px]'></SwiperSlide>
+            <SwiperSlide className='rounded-lg bg-[url("../public/wardrobe/pantalon5.png")] bg-cover bg-center max-h-[320px] min-w-[320px]'></SwiperSlide>
+          </Swiper>
         </div>
-        <p className="extra-info">
-          <span>
-            <label htmlFor="files" className="upload"> upload an image </label>
-            <input onChange={""} id="files" accept="image/*" type="file" hidden />
-          </span>
-          to ask questions about.
-        </p>
-        <p>What do you want to know about the image?
-          <button className="surprise" onClick={""} disabled={""}>Surprise me</button>
-        </p>
-        <div className="input-container">
+        <div className='flex w-full mx-auto p-6'>
           <input
             value={value}
-            placeholder="What is in the image..."
-            onChange={""}
+            placeholder='Que outfit necesitas?'
+            onChange={(e) => setValue(e.target.value)}
+            className='w-full rounded-l-lg border border-r-0 py-4 pl-4 focus:pr-4 text-xl outline-none'
           />
-          {(!response && !error) && <button onClick={""}>Ask me</button>}
-          {(response || error) && <button onClick={""}>Clear</button>}
+          {loading &&
+            <button className='w-[80px] flex justify-center items-center rounded-r-lg border px-2 text-xl'>
+              <div style={{ transform: 'scale(.75)' }}>
+                <ScaleLoader color='#ddd' />
+              </div>
+            </button>
+          }
+          {(!response && !error && !loading) && <button onClick={analyzeImage} className='w-[80px] flex justify-center items-center rounded-r-lg border px-2 text-xl'><SendIcon /></button>}
+          {(response || error) && <button onClick={clear} className='w-[80px] flex justify-center items-center rounded-r-lg border px-2 text-xl'>Clear</button>}
         </div>
-        {error && <p>{""}</p>}
-        {response && <p className="answer">{""}</p>}
+        {error && <p>{''}</p>}
+
+        {!response && loading && (
+          <div className='flex justify-center items-center mt-20'>
+            <ScaleLoader color='#ddd' />
+          </div>
+        )}
+        {response && <p className='p-6 text-lg mb-3'>{response}</p>}
       </section>
     </div>
   )
